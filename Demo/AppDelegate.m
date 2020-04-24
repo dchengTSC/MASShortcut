d0c9a444a5b9ae5c54804414adde2b448425b27e
David Cheng <davechengapps@gmail.com>
2020-04-24 08:48:24 -0400

    Fix demo build error

--- Demo/AppDelegate.m
+++ Demo/AppDelegate.m
@@ -63,7 +63,7 @@ static void *MASObservingContext = &MASObservingContext;
     [[NSSound soundNamed:@"Tink"] play];
     [_feedbackTextField setStringValue:NSLocalizedString(@"Shortcut unpressed!", @"Feedback that’s displayed when user presses the sample shortcut.")];
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [_feedbackTextField setStringValue:@""];
+        [self.feedbackTextField setStringValue:@""];
     });
 }
 
